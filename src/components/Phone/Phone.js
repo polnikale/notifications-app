8f49fcac3e629514c6aa6ccd3db2559d553b500a
unknown <nikpolale@gmail.com>
2018-06-19 14:56:09 +0300

    Добавил обрубку текста и двоеточие после него, если требуется

--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -5,6 +5,10 @@ import './Phone.css';
 function Phone(props) {
   const { heading, description, picture } = props;
   const pictureToDisplay = picture || defaultSvg;
+  const newDescription = renderText(description);
+  function renderText(text) {
+    return text.length <= 104 ? text : text.substr(0, 104).concat('...');
+  }
   return (
     <div className="phone">
       <figure className="notification">
@@ -14,7 +18,7 @@ function Phone(props) {
         />
         <figcaption>
           <h4>{heading}</h4>
-          <p>{description}</p>
+          <p>{newDescription}</p>
         </figcaption>
       </figure>
     </div>
