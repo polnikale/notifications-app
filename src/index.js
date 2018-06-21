cd7b3939b7645e3c74c233fa00093e6c4407c08b
unknown <nikpolale@gmail.com>
2018-06-21 18:56:45 +0300

    fixed returnBack issue

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
 import reducer from './reducers/index';
 import './index.css';
 import App from './components/App/App';
