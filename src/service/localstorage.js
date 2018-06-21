c69711d61c1aa78a48f69e217f504ab0c3a15dda
unknown <nikpolale@gmail.com>
2018-06-21 17:02:24 +0300

    copypasted service

---
+++ src/service/localstorage.js
@@ -0,0 +1,30 @@
+class LocalStorageService {
+    timeout(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async getNotifications() {
+      await this.timeout(1000);
+      const notifications = JSON.parse(localStorage.getItem('notifications')) || [];
+      return notifications;
+    }
+  
+    async appendNotification(notification) {
+      await this.timeout(1000)
+      const notifications = JSON.parse(localStorage.getItem('notifications')) || [];
+      localStorage.setItem('notifications', JSON.stringify([...notifications, notification]));
+    }
+  
+    async editNotification(notification, index) {
+      console.log(index);
+      await this.timeout(1000);
+      const notifications = JSON.parse(localStorage.getItem('notifications')) || [];
+      console.log([notifications.slice(0, index), notifications, notifications.slice(index+1)]);
+      this._appendToStorage( [ ...notifications.slice(0, index), notification, ...notifications.slice(index + 1) ] );
+    }
+  
+    _appendToStorage(notifications) {
+      localStorage.setItem('notifications', JSON.stringify(notifications));
+    }
+  }
+  
+  export default new LocalStorageService();
