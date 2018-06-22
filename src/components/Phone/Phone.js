e682d271070b79c7827f4c5c995e8b7c985594aa
unknown <nikpolale@gmail.com>
2018-06-22 15:01:19 +0300

    Got rid of Phone.css in favor of styled-components

--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import defaultSvg from './default.svg';
-import './Phone.css';
+import phoneSvg from './phone.svg';
+
+const PhoneWrapper = styled.div`
+  background-image: url(${phoneSvg});
+  object-fit: contain;
+  width: 231px;
+  height: 481px;
+  overflow: hidden;
+  padding: 20px;
+`;
+
+const Notification = styled.figure`
+  width: 178px;
+  min-height: 235px;
+  border-radius: 10px;
+  background-color: #ffffff;
+  box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.1), 0 0 20px 0 rgba(0, 0, 0, 0.02);
+  margin: 122px 29px;
+`;
+
+const Heading = styled.h4`
+  font-size: 10px;
+  color: #000;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 8px;
+  color: #9e9e9e;
+`;
+
+const NotificationImage = styled.img`
+  width: 178px;
+  object-fit: cover;
+  opacity: 0.95;
+  border-radius: 10px 10px 0 0;
+`;
+
+const NotificationText = styled.figcaption`
+  box-sizing: border-box;
+  text-align: center;
+  padding: 10px;
+`;
 
 class Phone extends React.Component {
   renderText(text) {
@@ -15,18 +58,18 @@ class Phone extends React.Component {
     const newDescription = this.renderText(description);
 
     return (
-      <div className="phone">
-        <figure className="notification">
-          <img 
+      <PhoneWrapper>
+        <Notification className="notification">
+          <NotificationImage 
             src={pictureToDisplay} 
             alt={pictureToDisplay} 
           />
-          <figcaption>
-            <h4>{heading}</h4>
-            <p>{newDescription}</p>
-          </figcaption>
-        </figure>
-      </div>
+          <NotificationText>
+            <Heading>{heading}</Heading>
+            <Description>{newDescription}</Description>
+          </NotificationText>
+        </Notification>
+      </PhoneWrapper>
     )
   }
 }
