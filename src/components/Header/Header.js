c09e591a1b62fa62b82676d5e682d3ce8bc90927
unknown <nikpolale@gmail.com>
2018-06-22 15:16:22 +0300

    got rid of header.css in favor of styled components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './Header.css';
 import leftArrow from './leftArrow.svg';
+
+const HeaderWrapper = styled.header`
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding-right: 10px;
+  align-items: center;
+  border-right: 2px solid rgba(40,44,55, .1);
+
+  &:after {
+    position: absolute;
+    content: '';
+    height: 2px;
+    width: 100%;
+    opacity: 0.1;
+    top: 80px;
+    left: 0;
+    background-color: #333333;
+  }
+`;
+
+const Heading = styled.h2`
+  margin-left: 20px;
+  display: inline-block;
+  font-weight: bold;
+`;
+
+const BackButton = styled.button`
+  height: 100%;
+  cursor: pointer;
+  width: 73px;
+  opacity: 0.5;
+  background-color: #ffffff;
+  border: none;
+`;
+
+const LeftSide = styled.div`
+  height: 100%;
+`;
 
 class Header extends React.Component {
   renderControls() {
     const { onBack } = this.props;
 
     return onBack && 
-    <button onClick={onBack} className="back">
+    <BackButton onClick={onBack} >
       <img src={leftArrow} alt="back" />
-    </button>
+    </BackButton>
   }
 
   render() {
@@ -19,13 +58,13 @@ class Header extends React.Component {
     const backButton = this.renderControls();
 
     return (
-      <header>
-        <div className="left">
+      <HeaderWrapper>
+        <LeftSide>
           {backButton}
-          <h2>{title}</h2>
-        </div>
+          <Heading>{title}</Heading>
+        </LeftSide>
         {children}
-      </header>
+      </HeaderWrapper>
     );
   }
 }
