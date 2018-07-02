292ace32c99a7e67dae309c89276fbbff678d4aa
unknown <nikpolale@gmail.com>
2018-07-02 14:54:30 +0300

    Changed app's behavior. Now notifications is Object

--- src/reducers/notifications.js
+++ src/reducers/notifications.js
@@ -2,23 +2,36 @@ import uuidv1 from 'uuid/v1';
 
 import { NOTIFICATION_SAVE } from '../actions/common';
 
-const initialState = [];
+const initialState = {};
 
 export const reducer = (state = initialState, action) => {
+  console.log(action.notification);
   switch (action.type) {
     case NOTIFICATION_SAVE:
       return (typeof action.notification.id === 'string')
-        ? state.map((notification, index) => (index === action.index
-          ? action.notification
-          : notification))
-        : [...state, {
-          ...action.notification,
-          id: uuidv1(),
-          time: Date.now(),
-        }];
+        ? {
+          ...state,
+          [action.notification.id]: {
+            heading: action.notification.heading,
+            description: action.notification.description,
+            pictures: action.notification.pictures,
+          },
+        }
+        : {
+          ...state,
+          [uuidv1()]: {
+            ...action.notification,
+            time: Date.now(),
+          },
+        };
     default:
       return state;
   }
 };
 
-export const getAllNotifications = state => state.notifications;
+export const getAllNotifications = state => Object.values(state.notifications).map((
+  (notification, index) => ({
+    ...notification,
+    id: Object.keys(state.notifications)[index],
+  })
+)).sort((arr1, arr2) => arr1.time < arr2.time);
